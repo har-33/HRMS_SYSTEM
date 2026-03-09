2511bb8c3572887971fce2223c1fb992eb8e225e
Harshad R <harshadr871@gmail.com>
2026-03-09 15:32:33 +0530

    feat: add Alembic migration system and user database model

---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from app.database.connection import Base
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password_hash = Column(String)
+    role = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
